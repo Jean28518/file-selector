5ae96a67f5d93202e3825c016718a83995bc59d4
Jean3219 <39700889+Jean28518@users.noreply.github.com>
2021-11-03 19:17:25 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Banner Skript
+
+This script simply let you select some files of a folder and copies this file to a specific location.\
+This is very useful for replacing the same file many times.\
+
+# How to use:
+
+In file_selector.conf you can specify the folder, from which all files can be selected.\
+You also can specify an output filepath.
+
+Start ./file_selector.py in your terminal. And follow the instructions.
---
+++ file_selector.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import jfiles
+import jfolders
+
+def main():
+    print("File Selector: Choose your file!")
+    while(True):
+        source_folder = jfiles.get_value_from_file("file_selector.conf", "source-folder", default=None)
+        entries = jfolders.get_folder_entries(source_folder)
+        for i in range(len(entries)):
+            print("[" + str(i) + "]\t" + entries[i].replace(source_folder, ""))
+        number = input("Number (q for quit): ")
+        if number == "q":
+            return
+        index = int(number)
+        if index < 0 or index >= len(entries):
+            print("!!!!Error: Number out of range!!!!")
+            continue
+        print("Choosing " + entries[index] + " ...")
+        jfiles.copy_file(entries[index], jfiles.get_value_from_file("file_selector.conf", "destination-file", default="output.png"))
+        print("--------------------------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()

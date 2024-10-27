b079e9740da117a0e997e01a70f3843edd56fe9d
Jean3219 <jf_v@gmx.de>
2024-10-27 14:41:46 +0100

    Update file_selector.py

--- file_selector.py
+++ file_selector.py
@@ -17,22 +17,20 @@ def main():
     # Call the function to change directory
     change_to_file_directory()
     print("File Selector: Choose your file!")
-    while(True):
-        source_folder = jfiles.get_value_from_file("file_selector.conf", "source-folder", default=None)
-        entries = jfolders.get_folder_entries(source_folder)
-        entries.sort()
-        for i in range(len(entries)):
-            print("[" + str(i) + "]\t" + entries[i].replace(source_folder+"/", ""))
-        number = input("Number (q for quit): ")
-        if number == "q":
-            return
-        index = int(number)
-        if index < 0 or index >= len(entries):
-            print("!!!!Error: Number out of range!!!!")
-            continue
-        print("Choosing " + entries[index] + " ...")
-        jfiles.copy_file(entries[index], jfiles.get_value_from_file("file_selector.conf", "destination-file", default="output.png"))
-        print("--------------------------------------------------------------")
+    source_folder = jfiles.get_value_from_file("file_selector.conf", "source-folder", default=None)
+    entries = jfolders.get_folder_entries(source_folder)
+    entries.sort()
+    for i in range(len(entries)):
+        print("[" + str(i) + "]\t" + entries[i].replace(source_folder+"/", ""))
+    number = input("Number (q for quit): ")
+    if number == "q":
+        return
+    index = int(number)
+    if index < 0 or index >= len(entries):
+        print("!!!!Error: Number out of range!!!!")
+    print("Choosing " + entries[index] + " ...")
+    jfiles.copy_file(entries[index], jfiles.get_value_from_file("file_selector.conf", "destination-file", default="output.png"))
+    print("--------------------------------------------------------------")
 
 
 if __name__ == "__main__":
